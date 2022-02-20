9c06de54050ddf8fa60646888c5f18ae6b296d08
ehayik <eduardo.eljaiek@gmail.com>
2022-02-20 18:40:48 +0200

    feat(account-query): events consumer

--- account-query/src/main/java/com/github/playground/techbank/account/query/AccountEventHandler.java
+++ account-query/src/main/java/com/github/playground/techbank/account/query/AccountEventHandler.java
@@ -1,9 +1,6 @@
 package com.github.playground.techbank.account.query;
 
-import com.github.playground.techbank.account.common.AccountClosedEvent;
-import com.github.playground.techbank.account.common.AccountOpenedEvent;
-import com.github.playground.techbank.account.common.FundsDepositedEvent;
-import com.github.playground.techbank.account.common.FundsWithdrawnEvent;
+import com.github.playground.techbank.account.common.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +16,16 @@ public class AccountEventHandler {
 
 	private final BankAccountRepository accountRepository;
 
-	public void on(@NonNull AccountOpenedEvent event) {
+	public void on(@NonNull AccountEvent event) {
+		switch (event) {
+			case AccountOpenedEvent evt -> on(evt);
+			case AccountClosedEvent evt -> on(evt);
+			case FundsDepositedEvent evt -> on(evt);
+			case FundsWithdrawnEvent evt -> on(evt);
+		}
+	}
+
+	private void on(AccountOpenedEvent event) {
 
 		if (accountRepository.existsById(event.getId())) {
 			throw new IllegalStateException("Account %s already exists".formatted(event.getId()));
@@ -36,7 +42,7 @@ public class AccountEventHandler {
 		accountRepository.save(bankAccount);
 	}
 
-	public void on(@NonNull FundsDepositedEvent event) {
+	private void on(FundsDepositedEvent event) {
 		accountRepository.findById(event.getId()).ifPresent(account -> {
 			LOG.info("Depositing funds to account {}", account.getId());
 			account.deposit(event.getAmount());
@@ -44,7 +50,7 @@ public class AccountEventHandler {
 		});
 	}
 
-	public void on(@NonNull FundsWithdrawnEvent event) {
+	private void on(FundsWithdrawnEvent event) {
 		accountRepository.findById(event.getId()).ifPresent(account -> {
 			LOG.info("Withdrawing funds from account {}", account.getId());
 			account.withdraw(event.getAmount());
@@ -53,7 +59,7 @@ public class AccountEventHandler {
 
 	}
 
-	public void on(@NonNull AccountClosedEvent event) {
+	private void on(AccountClosedEvent event) {
 		accountRepository.deleteById(event.getId());
 	}
 
